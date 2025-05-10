dea214412edde4f118ec8af95aaed7947c700db8
bhhxx <dreamj.9@sjtu.edu.cn>
2025-05-10 20:53:42 +0800

    level

---
+++ src/Level.hpp
@@ -0,0 +1,20 @@
+#pragma once // include guard
+#include <string>
+
+namespace aynlog {
+    class LogLevel {
+    public:
+        // enum class to avoid name conflicts with other libraries
+        enum class value { DEBUG, INFO, WARN, ERROR, FATAL};
+        static const char* ToString(value level) {
+            switch (level) {
+                case value::DEBUG: return "DEBUG"; // use value to show the enum value
+                case value::INFO: return "INFO";
+                case value::WARN: return "WARN";
+                case value::ERROR: return "ERROR";
+                case value::FATAL: return "FATAL";
+                default: return "UNKNOWN";
+            }
+        }
+    };
+}
